9d4a1a29f59f8af4d9b831a108f92052979a9cd4
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-06 23:14:18 +0700

    fix: syntax correction on validations

--- dto/login-dto.go
+++ dto/login-dto.go
@@ -2,5 +2,5 @@ package dto
 
 type LoginDTO struct {
 	Email    string `json:"email" form:"email" binding:"required,email"`
-	Password string `json:"password" form:"password" binding:"required,min:6"`
+	Password string `json:"password" form:"password" binding:"required,min=6"`
 }
