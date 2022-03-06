c3db88cd249cb70f5f7780ae428202b53e4c2b4e
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-06 23:07:43 +0700

    fix: validations

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -4,11 +4,11 @@ type UserUpdateDTO struct {
 	ID       uint64 `json:"id" form:"id" binding:"required"`
 	Name     string `json:"name" form:"name" binding:"required"`
 	Email    string `json:"email" form:"email" binding:"required"`
-	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
+	Password string `json:"password,omitempty" form:"password,omitempty" binding:"min:6"`
 }
 
 type UserCreateDTO struct {
 	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" binding:"required,min:6"`
 }
