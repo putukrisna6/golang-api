d19b77abe1ae8de917bd3aafec17dc2673350885
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-06 23:06:06 +0700

    fix: validation on email and password

--- dto/login-dto.go
+++ dto/login-dto.go
@@ -1,6 +1,6 @@
 package dto
 
 type LoginDTO struct {
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password" form:"password" binding:"requried" validate:"min:6"`
+	Email    string `json:"email" form:"email" binding:"required,email" validate:"email"`
+	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
 }
--- dto/register-dto.go
+++ dto/register-dto.go
@@ -2,6 +2,6 @@ package dto
 
 type RegisterDTO struct {
 	Name     string `json:"name" form:"name" binding:"required" validate:"min:1"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password" form:"password" binding:"requried" validate:"min:6"`
+	Email    string `json:"email" form:"email" binding:"required,email" validate:"email"`
+	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
 }
