4f3e2af1fd0695fbba44940aca76bcbc0b75fa7d
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-12 23:40:02 +0700

    feat: implement methods within book-service

--- service/book-service.go
+++ service/book-service.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/mashingan/smapping"
 	"github.com/putukrisna6/golang-api/dto"
 	"github.com/putukrisna6/golang-api/entity"
 	"github.com/putukrisna6/golang-api/repository"
@@ -9,42 +13,57 @@ import (
 type BookService interface {
 	Insert(b dto.BookCreateDTO) entity.Book
 	Update(b dto.BookUpdateDTO) entity.Book
-	Get(bookID string) entity.Book
+	Get(bookID uint64) entity.Book
 	Delete(b entity.Book)
 	All() []entity.Book
 	IsAllowedToEdit(userID string, bookID uint64) bool
 }
 
 type bookService struct {
-	bookReposity repository.BookRepository
+	bookRepository repository.BookRepository
 }
 
 func NewBookService(bookRepository repository.BookRepository) BookService {
 	return &bookService{
-		bookReposity: bookRepository,
+		bookRepository: bookRepository,
 	}
 }
 
 func (service *bookService) Insert(b dto.BookCreateDTO) entity.Book {
+	newBook := entity.Book{}
+	err := smapping.FillStruct(&newBook, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("failed to map: %v", err.Error())
+	}
 
+	res := service.bookRepository.InsertBook(newBook)
+	return res
 }
 
 func (service *bookService) Update(b dto.BookUpdateDTO) entity.Book {
+	book := entity.Book{}
+	err := smapping.FillStruct(&book, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("failed to map: %v", err.Error())
+	}
 
+	res := service.bookRepository.UpdateBook(book)
+	return res
 }
 
-func (service *bookService) Get(bookID string) entity.Book {
-
+func (service *bookService) Get(bookID uint64) entity.Book {
+	return service.bookRepository.GetBook(bookID)
 }
 
 func (service *bookService) Delete(b entity.Book) {
-
+	service.bookRepository.DeleteBook(b)
 }
 
 func (service *bookService) All() []entity.Book {
-
+	return service.bookRepository.AllBooks()
 }
 
 func (service *bookService) IsAllowedToEdit(userID string, bookID uint64) bool {
-
+	book := service.bookRepository.GetBook(bookID)
+	return userID == fmt.Sprintf("%v", book.UserID)
 }
