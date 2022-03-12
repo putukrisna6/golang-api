1de4a3d507d56756ba726083837325798c711930
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-12 21:54:56 +0700

    feat: create user-service

---
+++ service/user-service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"log"
+
+	"github.com/mashingan/smapping"
+	"github.com/putukrisna6/golang-api/dto"
+	"github.com/putukrisna6/golang-api/entity"
+	"github.com/putukrisna6/golang-api/repository"
+)
+
+type UserService interface {
+	Update(user dto.UserUpdateDTO) entity.User
+	Get(userID uint64) entity.User
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(userRepository repository.UserRepository) UserService {
+	return &userService{
+		userRepository: userRepository,
+	}
+}
+
+func (service *userService) Update(user dto.UserUpdateDTO) entity.User {
+	userToUpdate := entity.User{}
+	err := smapping.FillStruct(&userToUpdate, smapping.MapFields(user))
+	if err != nil {
+		log.Fatalf("failed to map %v: ", err)
+	}
+	updatedUser := service.userRepository.UpdateUser(userToUpdate)
+	return updatedUser
+}
+
+func (service *userService) Get(userID uint64) entity.User {
+	return service.userRepository.GetUser(userID)
+}
