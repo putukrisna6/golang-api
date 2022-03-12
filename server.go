a88f0a3cd4288202fcf1eb3859a96c901537d13c
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-03-13 00:47:55 +0700

    feat: routes for Book

--- server.go
+++ server.go
@@ -13,11 +13,16 @@ import (
 var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
-	jwtService     service.JWTService        = service.NewJWTService()
-	authService    service.AuthService       = service.NewAuthService(userRepository)
-	userService    service.UserService       = service.NewUserService(userRepository)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
+
+	jwtService  service.JWTService  = service.NewJWTService()
+	authService service.AuthService = service.NewAuthService(userRepository)
+	userService service.UserService = service.NewUserService(userRepository)
+	bookService service.BookService = service.NewBookService(bookRepository)
+
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
 	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	bookController controller.BookController = controller.NewBookController(bookService, jwtService)
 )
 
 func main() {
@@ -30,10 +35,19 @@ func main() {
 		authRoutes.POST("/register", authController.Register)
 	}
 
-	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	userRoutes := r.Group("api/users", middleware.AuthorizeJWT(jwtService))
 	{
 		userRoutes.GET("profile", userController.Get)
 		userRoutes.PUT("/", userController.Update)
+	}
+
+	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.Get)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
 	}
 
 	r.GET("/", func(c *gin.Context) {
