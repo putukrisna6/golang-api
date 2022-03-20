ec09379212c2fbdded182d6dc7848459ae12fbe9
Putu Krisna Andyartha <putukrisna6@gmail.com>
2022-03-20 14:56:12 +0700

    fix: bug causing multiple go containers to be created

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git pull origin master
 sudo docker stop golang-api
 sudo docker rm golang-api
 sudo docker build -t golang-api .
-sudo docker run --network="host" -p 8080:8080 -tid golang-api
+sudo docker run -d --name golang-api --network="host" -p 8080:8080 -tid golang-api
